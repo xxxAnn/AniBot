142f11da99f916625dedcda0528df0674e95dbe5
Kyando <candlelightwax@gmail.com>
2020-06-30 10:14:28 +0900

    .4

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -6,6 +6,32 @@ class Fun(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        
+    @commands.command()
+    async def kill(ctx, arg):
+        await ctx.send(arg + " is dead")
+
+    @commands.command()
+    async def suicide(ctx):
+        await ctx.send("https://www.mext.go.jp/a_menu/shotou/seitoshidou/1302907.htm")
+
+    @commands.command(pass_context=True)
+    async def cl(ctx, word1, word2):
+        word1 = str(word1)
+        word2 = str(word2)
+        x = []
+        for letter in word1:
+            for letter2 in word2:
+                if str.lower(letter2) == str.lower(letter):
+                    if letter in x:
+                        pass
+                    else:
+                        x.append(str.lower(letter))
+        v = str(x)
+        v = v.replace("'", "")
+        v = v.replace("[", "")
+        v = v.replace("]", "")
+        await ctx.send(v)
 
     @commands.command()
     async def emotify(self, ctx, *, arg):
@@ -21,6 +47,26 @@ class Fun(commands.Cog):
                 i = ":regional_indicator_" + i + ":"
                 x = x + " " + i
         await ctx.send(x)
+
+    @commands.command(pass_context=True)
+    async def pfp(ctx, *user: discord.Member):
+        r = False
+        if not user:
+            user = ctx.message.author
+            r = True
+        if not r:
+            user = user[0]
+        await ctx.send(user.avatar_url)
+
+    @commands.command(pass_context=True)
+    async def love(ctx, *tuple):
+        if len(tuple) > 1:
+            if tuple[0] == "Xuning" and tuple[1] == "Nancy":
+                await ctx.send("100" + "% Compatible")
+            else:
+                await ctx.send(str(random.randint(0, 100)) + "% Compatible")
+        else:
+            await ctx.send(str(random.randint(0,100)) + "% Compatible")
 
 
 def setup(bot):
--- cogs/Miscellaneous.py
+++ cogs/Miscellaneous.py
@@ -7,6 +7,56 @@ class Miscellaneous(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(pass_context=True)
+    async def members(ctx, rolename: discord.Role):
+        mm = ctx.message.guild.members
+        embed = discord.Embed(title="Members", description="in " + str(rolename), color=0x0d20a4)
+        for tm in mm:
+            if rolename in tm.roles:
+                embed.add_field(name=tm.display_name, value=tm.mention, inline=True)
+        await ctx.send(embed=embed)
+
+    @commands.command(pass_context=True)
+    async def warn(ctx, user: discord.Member, *cont):
+        global data
+        data = jsonLoad()
+        global mods
+        mods = jsonLoadMods()
+        print("command attempt")
+        if str(ctx.message.author.id) in mods:
+            w = False
+            if str(user.id) in mods:
+                if mods[str(user.id)] >= mods[str(ctx.message.author.id)]:
+                    print('this')
+                    w = True
+            if mods[str(ctx.message.author.id)] > 0 and not w:
+                stro = ""
+                for i in cont:
+                    stro = stro + i + " "
+                await user.send("You have been warned for the following reason: \n" + stro)
+                w = data[str(user.id)]
+                w["Warns"] = str(int(w["Warns"]) + 1)
+                x = str(user.id) + " has been warned for "+ stro + " on " + str(datetime.date(datetime.now())) + "\n"
+                with open("WarnLog.txt", "a") as f:
+                    f.write(x)
+                    f.close()
+                w = data[str(user.id)]
+                jsonUpdate()
+                await ctx.message.delete()
+            else:
+                await ctx.send("You do not have permission to do this")
+        else:
+            await ctx.send("You do not have permission to do this")
+
+    @commands.command(pass_context=True)
+    async def react(ctx, arg):
+        msg = ctx.message
+        await msg.add_reaction(arg)
+        x = await ctx.send(arg)
+        time.sleep(0.2)
+        await x.delete()
+        print(arg)
+
     @commands.command()
     async def online(self, ctx):
         mem = ctx.message.guild.members
